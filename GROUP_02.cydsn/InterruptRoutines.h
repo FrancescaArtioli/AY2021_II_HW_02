201c8575f8480ae6634f1c492335e3e8484f4c14
Francesca <francesca1.artioli@mail.polimi.it>
2021-04-05 21:16:14 +0200

    Adding files InterruptRoutines.h and RGBLedDriver.h

---
+++ GROUP_02.cydsn/InterruptRoutines.h
@@ -0,0 +1,26 @@
+/* ========================================
+ * \file InterruptRoutines.h
+ * Brief declaration of the two interrupt
+ * function we need, one for UART and one
+ * for the timer.
+ * ========================================
+*/
+    #define IDLE 0
+    #define HEADER 1
+    #define RED 2
+    #define GREEN 3
+    #define BLU 4
+    #define TAIL 5
+    
+#ifndef __INTERRUPT_ROUTINES_H__
+    #define __INTERRUPT_ROUTINES_H__
+    
+    //Include project header file
+    #include "project.h"
+
+    CY_ISR_PROTO(Custom_UART_RX_ISR);
+    CY_ISR_PROTO(Custom_TIMER_ISR);
+   
+   
+#endif
+/* [] END OF FILE */
