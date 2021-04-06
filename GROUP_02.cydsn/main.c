57254540793e411dbdbf70f1a81a05c499540b51
Francesca <francesca1.artioli@mail.polimi.it>
2021-04-06 17:35:41 +0200

    Adding connection command and indentation of main code

--- GROUP_02.cydsn/main.c
+++ GROUP_02.cydsn/main.c
@@ -13,76 +13,74 @@ volatile uint8_t status = 0;
 const Color BLACK = {0, 0, 0};
 volatile Color color;
 
-
+ static char message [20] = {'\0'};
 
 int main(void)
 {
     CyGlobalIntEnable; /* Enable global interrupts. */
-    UART_RGB_Start(); /*Enable Uart*/
+    
+    UART_RGB_Start(); /*Start Uart*/
     isr_UART_StartEx(Custom_UART_RX_ISR);
     RGBLed_Start();
+    
     status = 0;
+    
 	RGBLed_WriteColor(BLACK);
 
 	for (;;)
 	{
-	if (data_received==1 && status==0){ //IDLE
-	if (received==160){
-	status++;
-	data_received=0;
-	}
-	else {
-	status=0;
-	data_received=0;
-	}
+	    if (data_received==1 && status==0){ 
+            if (received==160){
+                status++;
+                data_received=0;
+            }
+            
+            else if (received=='v'){
+                sprintf(message, "RGB LED Program $$$");
+                UART_RGB_PutString(message);
+                data_received=0;
+                status = 0;
+            }
+            else {
+                status=0;
+                data_received=0;
+            }
+        }
+        
+        else if (data_received==1 && status==1){
+            color.red = received;
+            status++;
+            data_received=0;
+        }
+        
+        else if (data_received==1 && status==2){
+            color.green=received;
+            status++;
+            data_received=0;
+        }
+        
+        else if (data_received==1 && status==3){
+            color.blu=received;
+            status++;
+            data_received=0;
+        }
+        
+        else if (data_received==1 && status==4){
+            if (received==192){
+                status++;
+            }
+            else {
+                status=0;
+                data_received=0;
+            }
+        }
+        
+        else if (status==5){
+            RGBLed_WriteColor(color);
+            status=0;
+            data_received=0;
+        }
     }
-
-	
- 
-    else if (data_received==1 && status==1){
-	color.red = received;
-	status++;
-	data_received=0;
-	}
-	
-	else if (data_received==1 && status==2){
-	color.green=received;
-	status++;
-	data_received=0;
-	}
-	
-	
-	else if (data_received==1 && status==3){
-	color.blu=received;
-	status++;
-	data_received=0;
-	}
-	
-	
-	else if (data_received==1 && status==4){
-	if (received==192){
-	status++;
-	}
-	else {
-        status=0;
-        data_received=0;
-    }
-    }
-	
-	
-	else if (status==5){
-	RGBLed_WriteColor(color);
-	status=0;
-	data_received=0;
-	}
-	
-	
-    
 }
-}
-
-	
-   
-
 
 /* [] END OF FILE */
